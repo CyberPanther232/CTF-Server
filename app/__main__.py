e8a43e1339fe4379c1b6148994955226a2e58955
CyberPanther232 <kinneyh27@gmail.com>
2025-10-22 23:38:59 -0400

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   app/__main__.py
    	modified:   main.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,9 @@
+import os
 from . import app
 
 if __name__ == "__main__":
-    app.run(debug=app.config.get("PRODUCTION_MODE", False))
+    # Listen on all interfaces so it's reachable from Docker/other hosts
+    host = os.environ.get("HOST", "0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    debug = bool(app.config.get("DEVELOPMENT_MODE", False))
+    app.run(host=host, port=port, debug=debug)
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=app.config.get('DEVELOPMENT_MODE', False))
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    debug = bool(app.config.get('DEVELOPMENT_MODE', False))
+    app.run(host=host, port=port, debug=debug)
