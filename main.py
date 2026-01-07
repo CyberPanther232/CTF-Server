9d5641b8aefcd50096a6e6f089ef644d26869c1b
Hunter <118036812+CyberPanther232@users.noreply.github.com>
2026-01-07 11:00:54 -0500

    Change default host to 'localhost' and port to 5001

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from app import app
 
 if __name__ == '__main__':
-    host = os.environ.get('HOST', '0.0.0.0')
-    port = int(os.environ.get('PORT', 5000))
+    host = os.environ.get('HOST', 'localhost')
+    port = int(os.environ.get('PORT', 5001))
     debug = bool(app.config.get('DEVELOPMENT_MODE', False))
     # Disable the Flask reloader when running in Docker
     app.run(host=host, port=port, debug=debug, use_reloader=False)
